c56322878e1bc5050a54141b17dc4aee7e85afda
Muhammad Ahmed <ahmednoorani258@gmail.com>
2024-03-22 14:19:31 -0700

    update 2.0

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+#!/usr/bin/env node
+console.log(`Welcome to Noorani Number_Guessing_Game`);
 import inquirer from "inquirer";
-//1)computer will generate a random number
-let randomnum = Math.floor(Math.random() * 100 + 1);
-//2)user input for guessing number
-const GuessNumber = await inquirer.prompt([
-    {
+let randomnum = Math.floor(Math.random() * 10 + 1);
+while (true) {
+    let input = await inquirer.prompt({
         name: "GuessedNumber",
         type: "number",
-        message: "Pls guess the number between 1 - 100."
+        message: "Pls guess the number between 1 - 100:"
+    });
+    let guessednum = input.GuessedNumber;
+    //3)compare user input with computer number
+    if (input.GuessedNumber === randomnum) {
+        console.log("Congratulation!) You got it right!");
+        break;
     }
-]);
-console.log(GuessNumber);
-//3)compare user input with computer number
-if (GuessNumber.GuessedNumber === randomnum) {
-    console.log("Congratulation!) You got it right!");
-}
-else if (GuessNumber.GuessedNumber > randomnum) {
-    console.log('Your number is too high');
-}
-else if (GuessNumber.GuessedNumber < randomnum) {
-    console.log(`your number is to low`);
+    else {
+        console.log(`wrong guess`);
+    }
+    if (input.GuessedNumber > randomnum) {
+        console.log('Your number is too high');
+    }
+    else {
+        console.log("Your number is too low");
+    }
 }
--- index.ts
+++ index.ts
@@ -1,28 +1,38 @@
+#!/usr/bin/env node
+
+console.log(`Welcome to Noorani Number_Guessing_Game`);
+
 import inquirer from "inquirer"
 
-//1)computer will generate a random number
-let randomnum = Math.floor(Math.random()*100 + 1);
-//2)user input for guessing number
-const GuessNumber = await inquirer.prompt([
+let randomnum:number = Math.floor(Math.random()*100 + 1);
+
+while(true){
+    let input = await inquirer.prompt(
     {
         name: "GuessedNumber",
         type: "number",
         message: "Pls guess the number between 1 - 100:"
     }
-])
+)
 
-console.log(GuessNumber);
+
+let guessednum = input.GuessedNumber;
 //3)compare user input with computer number
 
-if(GuessNumber.GuessedNumber === randomnum){
+if(input.GuessedNumber === randomnum){
     console.log("Congratulation!) You got it right!")
-
-}else if (GuessNumber.GuessedNumber > randomnum){
-    console.log('Your number is too high')
-    
-}else if (GuessNumber.GuessedNumber < randomnum){
-    console.log(`your number is to low`);
+    break;
+}else {console.log(`wrong guess`);
 }
+
+ if (input.GuessedNumber > randomnum){
+    console.log('Your number is too high')
+}else{console.log("Your number is too low");
+}
+}
+
+console.log(`Game over`);
+
 
 
 
